3eac8d9f1f210f7598d49f6a2b2f0391bb7c0955
ningmaple <ningmaple7386@gmail.com>
2023-05-18 19:38:18 -0700

    LC.844 Backspace String Compare
    Added src code.

---
+++ backspace_string_compare/BackspaceStringCompare.java
@@ -0,0 +1,94 @@
+public class BackspaceCompare {
+    public boolean backspaceCompare(String s, String t) {
+        if (s.equals(t)) {
+            return true;
+        }
+        if (s == null || t == null || s.length() == 0 || t.length() == 0) {
+            return false;
+        }
+        
+        int sptr = s.length() - 1;
+        int tptr = t.length() - 1;
+        while (sptr >= 0 && tptr >= 0) {
+            char sChar = s.charAt(sptr);
+            char tChar = t.charAt(tptr);
+            if (sChar == '#') {
+                int counter = 1;
+                sptr--;
+                while (counter > 0 && sptr >= 0) {
+                    sChar = s.charAt(sptr);
+                    if (sChar == '#') {
+                        counter++;
+                    } else {
+                        counter--;
+                    }
+                    sptr--;
+                }
+                continue;
+            }
+
+            if (tChar == '#') {
+                int counter = 1;
+                tptr--;
+                while (counter > 0 && tptr >= 0) {
+                    tChar = t.charAt(tptr);
+                    if (tChar == '#') {
+                        counter++;
+                    } else {
+                        counter--;
+                    }
+                    tptr--;
+                }
+                continue;
+            }
+
+            if (sChar == tChar) {
+                tptr--;
+                sptr--;
+                continue;
+            }
+
+            return false;
+        }
+        
+        while (sptr >= 0) {
+            char sChar = s.charAt(sptr);
+            if (sChar == '#') {
+                int counter = 1;
+                sptr--;
+                while (counter > 0 && sptr >= 0) {
+                    sChar = s.charAt(sptr);
+                    if (sChar == '#') {
+                        counter++;
+                    } else {
+                        counter--;
+                    }
+                    sptr--;
+                }
+                continue;
+            }
+            break;
+        }
+        
+        while (tptr >= 0) {
+            char tChar = t.charAt(tptr);
+            if (tChar == '#') {
+                int counter = 1;
+                tptr--;
+                while (counter > 0 && tptr >= 0) {
+                    tChar = t.charAt(tptr);
+                    if (tChar == '#') {
+                        counter++;
+                    } else {
+                        counter--;
+                    }
+                    tptr--;
+                }
+                continue;
+            }
+            break;
+        }
+        
+        return sptr < 0 && tptr < 0;
+    }
+}
